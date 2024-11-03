6b71a1789a2973556f79012d231d895b76bbbfc4
EGOR-18 <egorpi005@gmail.com>
2024-11-03 10:17:11 +0000

    module_3_hard.py change

--- module_3_hard.py
+++ module_3_hard.py
@@ -6,7 +6,7 @@ def calculate_structure_sum(data):
             total_sum += calculate_structure_sum(key)
             total_sum += calculate_structure_sum(value)
 
-    elif isinstance(data, (list, tuple)):
+    elif isinstance(data, (list, tuple, set)):
         for item in data:
             total_sum += calculate_structure_sum(item)
 
@@ -19,12 +19,6 @@ def calculate_structure_sum(data):
     return total_sum
 
 
-data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
+data_structure = (2.0, "Nicholas", {1, 2, 3})
 result = calculate_structure_sum(data_structure)
 print(result)
