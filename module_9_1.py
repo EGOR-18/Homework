57dad7760c4aaf98560f6a5422fa745bbe88e31a
EGOR-18 <egorpi005@gmail.com>
2024-11-04 20:03:19 +0000

    module_9_1.py

---
+++ module_9_1.py
@@ -0,0 +1,8 @@
+def apply_all_func(int_list, *functions):
+    dict_int = {}
+    for func in functions:
+        dict_int[func] = func(int_list)
+    return dict_int
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
