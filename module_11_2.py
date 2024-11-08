0482e7a0cc0c660229894b7041a4fc312c1218f0
EGOR-18 <egorpi005@gmail.com>
2024-11-08 20:30:52 +0000

    module_11_2.py

---
+++ module_11_2.py
@@ -0,0 +1,19 @@
+import inspect
+
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith('__')]
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith('__')]
+    obj_module = getattr(obj, '__module__', None)
+
+    info = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': obj_module,
+    }
+    return info
+
+
+number_info = introspection_info("ddd")
+print(number_info)
