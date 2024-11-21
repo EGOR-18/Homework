6ac8a11fbcb66245c4577ba2fa212d9a9f941f68
EGOR-18 <egorpi005@gmail.com>
2024-11-21 22:11:25 +0000

    module_14_5.py

--- crud_functions.py
+++ crud_functions.py
@@ -9,10 +9,17 @@ def initiate_db():
     description TEXT,
     price INTEGER NOT NULL
     )''')
-    connection.commit()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )''')
+
     # for i in range(1,5):
     #    cursor.execute('INSERT INTO Products(title, description, price) VALUES(?,?,?)', (f"Продукт{i}", f"Описание{i}", f"{i*100}"))
-    #connection.commit()
+    connection.commit()
     connection.close()
 
 def get_all_products(id1):
@@ -25,5 +32,18 @@ def get_all_products(id1):
     id, title, description, price = prod[0]
     return f"Название: {title} | Описание: {description} | Цена: {price}"
 
+def add_user(username, email, age):
+    connection = sqlite3.connect("telegram.db")
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?,?,?,?)',(f"{username}", f"{email}", f"{age}", "1000"))
+    connection.commit()
+    connection.close()
 
+def is_included(username):
+    connection = sqlite3.connect("telegram.db")
+    cursor = connection.cursor()
+    if cursor.execute("SELECT * FROM Users WHERE username = ?", (username,)).fetchall():
+        return True
+    return False
 
+add_user("egor", "1", "12")
