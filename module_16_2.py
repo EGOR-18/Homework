087a2fa6370022f33a421b957d5465f0da362336
EGOR-18 <egorpi005@gmail.com>
2024-12-09 21:04:59 +0000

    module_16_2.py 3

--- module_16_2.py
+++ module_16_2.py
@@ -20,7 +20,7 @@ async def get_user_info(
     username: Annotated[str,  Path(min_length=5, max_length=20, description='Enter username')],
     age:  Annotated[int, Path(ge=18, le=120, description='Enter age')]
     ):
-    return {"username": username, "age": age}
+    return f"username: {username}, age: {age}"
 
 @app.get("/user")
 async def user_info(username: str, age: int) -> str:
