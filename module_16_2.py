583df591401f81da1f67e7855f533e4120358520
EGOR-18 <egorpi005@gmail.com>
2024-12-09 21:03:13 +0000

    module_16_2.py 2

--- module_16_2.py
+++ module_16_2.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
+
 app = FastAPI()
 
-@app.get('/user/{user_id}')
-async def get_user(user_id: int = Path(ge=1, le=100, description='Enter User ID')):
-    return {"user_id": user_id}
+@app.get("/")
+async def main() -> str:
+    return "Главная страница"
+
+@app.get("/user/admin")
+async def admin() -> str:
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+async def get_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')]) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get('/user/{username}/{age}')
 async def get_user_info(
     username: Annotated[str,  Path(min_length=5, max_length=20, description='Enter username')],
-    age: int = Path(ge=18, le=120, description='Enter age')
-):
+    age:  Annotated[int, Path(ge=18, le=120, description='Enter age')]
+    ):
     return {"username": username, "age": age}
+
+@app.get("/user")
+async def user_info(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
+
+
+
