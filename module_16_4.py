72988449facf528b7aa74381cba0503b171c9d2f
EGOR-18 <egorpi005@gmail.com>
2024-12-12 20:27:26 +0000

    module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -17,14 +17,17 @@ async def get_all_users():
     return users
 
 @app.post("/user/{username}/{age}", response_model=Users)
-async def add_user(username: str, age: int):
+async def add_user(username: Annotated[str,  Path(min_length=5, max_length=20, description='Enter username')],
+                   age:  Annotated[int, Path(ge=18, le=120, description='Enter age')]):
     user_id = len(users) + 1
     user = Users(username=username, age=age, id=user_id)
     users.append(user)
     return user
 
 @app.put("/user/{user_id}/{username}/{age}", response_model=Users)
-async def update_user(user_id: int, username: str, age: int):
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')],
+                      username: Annotated[str,  Path(min_length=5, max_length=20, description='Enter username')],
+                      age:  Annotated[int, Path(ge=18, le=120, description='Enter age')]):
     for user in users:
         if user.id == user_id:
             user.username = username
@@ -33,7 +36,7 @@ async def update_user(user_id: int, username: str, age: int):
     raise HTTPException(status_code=404 ,detail="User was not found")
 
 @app.delete("/user/{user_id}", response_model=Users)
-async def delete_user(user_id: int):
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')]):
     for user in users:
         if user.id == user_id:
             users.remove(user)
